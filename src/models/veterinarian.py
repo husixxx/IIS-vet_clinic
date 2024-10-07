c24fca67371c421bb64b638f683506d0ff8d9b63
husixxx <xhuska03@stud.fit.vutbr.cz>
2024-10-07 11:16:05 +0200

    Fix veterinarians table

--- src/models/veterinarian.py
+++ src/models/veterinarian.py
@@ -1,10 +1,10 @@
 from . import db
 from .animal import Animal
 
-class Caretaker(db.Model):
-    __tablename__ = 'caretakers'
+class Veterinarian(db.Model):
+    __tablename__ = 'veterinarians'
     id = db.Column(db.Integer, db.ForeignKey('persons.id'), primary_key=True)
-    managed_animals = db.relationship('Animal', backref='caretaker', lazy=True)
+    treated_animals = db.relationship('MedicalRecord', backref='veterinarian', lazy=True)
 
     def __repr__(self):
-        return f'<Caretaker ID {self.id}>'
+        return f'<Veterinarian ID {self.id}>'
