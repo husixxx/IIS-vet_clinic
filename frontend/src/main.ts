bc071b9d9b1cedb3c41b66aec7954defab181b18
Andrii <and.klimenkoo@gmail.com>
2024-10-12 21:46:55 +0200

    refactoring: moved style.css to assets

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import './style.css';
+import './assets/style.css';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
