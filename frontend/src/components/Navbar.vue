09ee4b6c939a9da5254325b6c68ecbe4ed0cc32a
Andrii <and.klimenkoo@gmail.com>
2024-10-12 22:31:35 +0200

    removed icons from the Navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -14,27 +14,22 @@ const router = useRouter();  // Use Vue Router for navigation
 const items = ref([
 {
     label: 'Home',
-    icon: 'pi pi-home',
     command: () => { router.push({name: "Home"}) }  // Route to Home
 },
 {
     label: 'Animals',
-    icon: 'pi pi-paw',
     command: () => { router.push({name: "Animal"}) }  // Route to Animals
 },
 {
     label: 'About Us',
-    icon: 'pi pi-info-circle',
     command: () => { router.push({name: "About"}) }  // Route to About Us
 },
 {
     label: 'Contact Us',
-    icon: 'pi pi-envelope',
     command: () => { router.push({name: "Contact"}) }  // Route to Contact Us
 },
 {
     label: 'Sign In',
-    icon: 'pi pi-sign-in',
     command: () => { router.push({name: "Sign"}) }  // Route to Sign In
 }
 ]);
