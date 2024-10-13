6b622a1296153de776409297c4c8ae10443bd450
xjanek05 <xjanek05@fit.stud.vutbr.cz>
2024-10-13 14:35:49 +0200

    Add auth store

--- backend/src/app.py
+++ backend/src/app.py
@@ -13,8 +13,12 @@ from flask_cors import *
 def create_app():
   app = Flask(__name__)
 
-  CORS(app, resources={r"/*": {"origins": ["http://localhost:5173"]}})
-
+  CORS(app, resources={
+    r"/*": {  # Allow all routes
+        "origins": ["http://127.0.0.1:5173", "http://localhost:5342", "http://localhost:5000"],
+        "supports_credentials": True
+      }
+  })
   # Database
   DATABASE_URI = 'postgresql://husic:husic@postgres:5432/iis'
   app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
