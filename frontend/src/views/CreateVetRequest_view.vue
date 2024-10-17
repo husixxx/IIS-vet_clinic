47ff205ddd4ccdf19a8709bbeb24580208c6d364
Andrii <and.klimenkoo@gmail.com>
2024-10-17 22:02:15 +0200

    updated CreateVetRequest view: now caretaker can select a date

--- frontend/src/views/CreateVetRequest_view.vue
+++ frontend/src/views/CreateVetRequest_view.vue
@@ -16,10 +16,14 @@
             <label for="description" class="input-label">Description</label>
             <Textarea id="description" v-model="description" placeholder="Enter request's description" class="input-text" rows="5" />
           </div>
+          <div class="p-field input-group">
+            <label for="date" class="input-label">Date</label>
+            <DatePicker class="input-text" v-model="date" :manualInput="false" dateFormat="yy-mm-dd" showIcon fluid iconDisplay="input" />
+          </div>
           <div class="p-field send-vet-request">
-            <Button label="Send request" @click="handleSendVetRequest" class="w-full" />
+            <Button label="Send request" @click="handleSendVetRequest" class="w-full" icon="pi pi-send" />
           </div>
-          </div>
+        </div>
       </template>
     </Card>
   </div>
@@ -33,12 +37,16 @@ import Button from 'primevue/button';
 import axiosClient from '../api/api';
 import AutoComplete  from 'primevue/autocomplete';
 import Textarea from 'primevue/textarea';
+import DatePicker from 'primevue/datepicker';
+import 'primeicons/primeicons.css'
 
 const veterinarians = ref([]);
 const filteredVeterinarians = ref([]);
 const selectedVeterinarian = ref(null);
 const animalId = ref(7); // TODO CHANGE IT!!!
 const description = ref('');
+const date = ref(null);
+const SUCCESS_RESPONSE_CODE = ref(200);
 
 onMounted(async () => {
     try {
@@ -60,28 +68,36 @@ const searchVeterinarians = (event) => {
   );
 };
 
-function getCurrentDate() {
-  const today = new Date();
+// function getCurrentDate() {
+//   const today = new Date();
 
-  const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, '0');
-  const day = String(today.getDate()).padStart(2, '0');
+//   const year = today.getFullYear();
+//   const month = String(today.getMonth() + 1).padStart(2, '0');
+//   const day = String(today.getDate()).padStart(2, '0');
 
-  return  `${year}-${month}-${day}`;
+//   return  `${year}-${month}-${day}`;
+// }
+
+function getFormattedDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
 }
 
 const handleSendVetRequest = async () => {
   try {
+    console.log(date.value);
     const response = await axiosClient.post(
       `/caretaker/vet_request?animal_id=${encodeURIComponent(animalId.value)}` + 
       `&veterinarian_username=${encodeURIComponent(selectedVeterinarian.value.username)}` +
-      `&request_date=${encodeURIComponent(getCurrentDate())}&description=${encodeURIComponent(description.value)}`
+      `&request_date=${encodeURIComponent(getFormattedDate(date.value))}&description=${encodeURIComponent(description.value)}`
     );
 
-    if(response.status === 200) {
+    if(response.status === SUCCESS_RESPONSE_CODE.value) {
       selectedVeterinarian.value = '';
       description.value = '';
-
+      date.value = '';
       alert('Veterinarian request created successfully!');
     }
 
