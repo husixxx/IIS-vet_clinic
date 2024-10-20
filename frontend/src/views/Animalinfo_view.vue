830422d8ee0e775fcf4882ae4316e5ae291af9f2
Andrii <and.klimenkoo@gmail.com>
2024-10-20 18:31:45 +0200

    now caretaker can adit animal's schedule (but it doesn't work)

--- frontend/src/views/Animalinfo_view.vue
+++ frontend/src/views/Animalinfo_view.vue
@@ -15,7 +15,7 @@
             <DataTable :value="animalSchedules" class="full-width-table" tableStyle="width: 100%">
               <Column field="start_time" header="Start date" style="width: 10%;"></Column>
               <Column field="end_time" header="End date" style="width: 10%;"></Column>
-              <Column v-if="authStore.getRoleId === UserRole.Volunteer" header="" style="width: 10%; text-align: right;" headerStyle="text-align: right; padding-right: 30px;">
+              <Column v-if="authStore.getRoleId === UserRole.Volunteer" header="" style="width: 5%; text-align: right;" headerStyle="text-align: right; padding-right: 30px;">
                 <template #body="slotProps">
                   <Button
                     label="Make reservation"
@@ -24,11 +24,11 @@
                   />
                 </template>
               </Column>
-              <Column v-if="authStore.getRoleId === UserRole.Caretaker" header="" style="width: 10%; text-align: right;" headerStyle="text-align: right; padding-right: 30px;">
+              <Column v-if="authStore.getRoleId === UserRole.Caretaker" header="" style="width: 5%; text-align: right;" headerStyle="text-align: right; padding-right: 30px;">
                 <template #body="slotProps">
                   <Button
                     label="Edit schedule"
-                   
+                    @click="openScheduleEditModal(slotProps.data)"
                     class="p-button-warning"
                   />
                 </template>
@@ -94,11 +94,31 @@
       </Card>
     </div>
   </div>
+
+  <Dialog header="Edit schedule" v-model:visible="showScheduleEditDialog" :modal="true" :closable="true" :style="{ width: '50vw' }">
+    <div class="p-fluid">
+      <div class="p-field" v-for="field in reservSchedulesFields" :key="field.id">
+        <label :for="field.id" class="input-label">{{ field.label }}</label>
+        <component
+          :is="field.component"
+          v-model="selectedSchedule[field.model]"
+          v-bind="field.props"
+          :id="field.id"
+          class="input-field"
+        />
+      </div>
+    </div>
+    <template #footer>
+      <Button label="Save" @click="updateSchedule" class="p-button-success" />
+      <Button label="Cancel" @click="closeScheduleEditModal" class="p-button-secondary" />
+    </template>
+  </Dialog>
+
 </template>
   
 <script setup>
 
-import { ref, onMounted } from 'vue';
+import { ref, reactive, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import axiosClient from '../api/api';
 import { useAuthStore, UserRole } from '../store/Authstore';  // Ensure correct import of your AuthStore
@@ -107,6 +127,8 @@ import DataTable from 'primevue/datatable';
 import Card from 'primevue/card';
 import Column from 'primevue/column';
 import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
+import DatePicker from 'primevue/datepicker';
 
 const route = useRoute();
 const authStore = useAuthStore();
@@ -116,30 +138,100 @@ const animalInfo = ref();
 const animalSchedules = ref([]);
 const animalMedicalRecords = ref([]);
 
-// const showEditDialog = ref(false);
-// const selectedSchedule = reactive({
-//   id: null,
-//   animalId: null,
-//   startTime: '',
-//   endTime: ''
-// });
+const showScheduleEditDialog = ref(false);
+const selectedSchedule = reactive({
+  id: null,
+  // animalId: null,
+  startDate: '',
+  endDate: ''
+});
 
-// const reservSchedulesFields = [
-//   { id: 'startDate', label: 'Start date', model: 'startDate', component: InputText },
-//   { id: 'endtDate', label: 'endDate', model: 'endDate', component: InputText }
-//   // { id: 'username', label: 'Username', model: 'username', component: InputText },
-//   // { id: 'password', label: 'Password', model: 'password', component: InputText, props: { type: 'password' } },
-//   // { id: 'verified', label: 'Verified', model: 'verified', component: Dropdown, props: { options: verifiedOptions, optionLabel: 'label' } },
-//   // { id: 'role_id', label: 'Role', model: 'role_id', component: Dropdown, props: { options: roleOptions, optionLabel: 'label' } }
-// ];
+const reservSchedulesFields = [
+  { 
+    id: 'startDate',
+    label: 'Start date',
+    model: 'startDate',
+    component: DatePicker,
+    props: 
+    {
+      // model: 'scheduleStartTime',
+      dateFormat: "yy-mm-dd",
+      showTime: true,
+      showSeconds: true,
+      hourFormat: '24'
+    }
+  },
+  { 
+    id: 'endtDate',
+    label: 'End date ',
+    model: 'endDate',
+    component: DatePicker,
+    props: 
+    {
+      // model: 'scheduleEndTime',
+      dateFormat: "yy-mm-dd",
+      showTime: true,
+      showSeconds: true,
+      hourFormat: '24'
+    }
+  }
+];
+
+function formatDate(inputDate) {
+    const date = new Date(inputDate);
+
+    const year = date.getUTCFullYear();
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+    const day = String(date.getUTCDate()).padStart(2, '0');
+    const hours = String(date.getUTCHours()).padStart(2, '0');
+    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(date.getUTCSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+const updateSchedule = async () => {
+  try {
+    console.log(`/caretaker/update_walking_schedule?walking_schedule_id=${encodeURIComponent(selectedSchedule.id)}` +
+                                           `&start_time=${encodeURIComponent(formatDate(selectedSchedule.startDate))}` +
+                                           `&end_time=${encodeURIComponent(formatDate(selectedSchedule.endDate))}`);
+    console.log(`/caretaker/update_walking_schedule?walking_schedule_id=${selectedSchedule.id}` +
+                                           `&start_time=${formatDate(selectedSchedule.startDate)}` +
+                                           `&end_time=${formatDate(selectedSchedule.endDate)}`);
+    const response = await axiosClient.put(`/caretaker/update_walking_schedule?walking_schedule_id=${encodeURIComponent(selectedSchedule.id)}` +
+                                           `&start_time=${encodeURIComponent(formatDate(selectedSchedule.startTime))}` +
+                                           `&end_time=${encodeURIComponent(formatDate(selectedSchedule.endTime))}`);
+    if (response.status === 200) {
+      alert('Schedule updated successfully!');
+      closeScheduleEditModal();
+      onMounted();
+    }
+  } catch (error) {
+    console.error('Error updating schedule:', error);
+    alert('Failed to update schedule.');
+  }
+};
+
+const openScheduleEditModal = async (schedule) => {
+  selectedSchedule.startTime = schedule.start_time;
+  selectedSchedule.endTime = schedule.end_time;
+  selectedSchedule.id = schedule.id;
+  // console.log('Mounted: ', selectedSchedule);
+  showScheduleEditDialog.value = true;
+};
+
+const closeScheduleEditModal = async () => {
+  showScheduleEditDialog.value = false;
+};
 
 onMounted(async () => {
   try {
     const response = await axiosClient.get(`/animal/info?animal_id=${encodeURIComponent(route.params.id)}`);
-    console.log(response.data);
+    // console.log(response.data);
     animalInfo.value = response.data;
     animalSchedules.value = animalInfo.value.schedules;
     animalMedicalRecords.value = animalInfo.value.medical_records;
+    // console.log('schedules: ', animalSchedules.value);
   } catch (error) {
     console.error('Error fetching animal data:', error);
   }
@@ -229,13 +321,13 @@ fieldset {
 }
 
 .full-width-table .p-column-title, .full-width-table td {
-    white-space: normal;
-    word-break: break-word;
+  white-space: normal;
+  word-break: break-word;
 }
 
 .full-width-table td {
-    max-width: 100%; /* Ensure cells don't stretch beyond available space */
-    padding: 10px;
+  max-width: 100%; /* Ensure cells don't stretch beyond available space */
+  padding: 10px;
 }
 
 </style>
