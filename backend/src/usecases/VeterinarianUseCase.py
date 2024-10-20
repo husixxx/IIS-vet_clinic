f3809919e3922353214d1c42a971be7ea580d3e5
Andrii <and.klimenkoo@gmail.com>
2024-10-20 17:19:52 +0200

    added new veterinarian use case: update_medical_record()

--- backend/src/usecases/VeterinarianUseCase.py
+++ backend/src/usecases/VeterinarianUseCase.py
@@ -32,6 +32,22 @@ class VeterinarianUseCase:
         # self.public_repository.add(new_medical_record)
         self.medical_record_repository.add(new_medical_record)
         return new_medical_record
+    
+    def update_medical_record(self, medical_record_id: int, veterinarian_id: int, examination_date: str, examination_type: str, description: str):
+        
+        medical_record = self.medical_record_repository.get_by_id(medical_record_id)
+
+        if not medical_record:
+            raise ValueError("Medical record not found")
+        
+        if int(medical_record.veterinarian_id) != int(veterinarian_id):
+            raise ValueError("You can modify only your medical records")
+        
+        medical_record.examination_date = examination_date
+        medical_record.examination_type = examination_type
+        medical_record.description = description
+        self.medical_record_repository.update(medical_record)
+
         
         
         
