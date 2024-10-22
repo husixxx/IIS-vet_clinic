e82f150edf1e46f977e2ad102e8e71bf2c43773a
Andrii <and.klimenkoo@gmail.com>
2024-10-22 02:12:23 +0200

    fixed date util

--- frontend/src/utils/date.ts
+++ frontend/src/utils/date.ts
@@ -1,9 +1,9 @@
 export function getFormattedDate(inputDate: string, needTime: boolean = false): string {
     const date = new Date(inputDate);
 
-    const year = date.getUTCFullYear();
-    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-    const day = String(date.getUTCDate()).padStart(2, '0');
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
 
     const dateStr = `${year}-${month}-${day}`;
 
@@ -11,9 +11,9 @@ export function getFormattedDate(inputDate: string, needTime: boolean = false): 
         return dateStr;
     }
 
-    const hours = String(date.getUTCHours()).padStart(2, '0');
-    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-    const seconds = String(date.getUTCSeconds()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
 
     return `${dateStr} ${hours}:${minutes}:${seconds}`;
 }
