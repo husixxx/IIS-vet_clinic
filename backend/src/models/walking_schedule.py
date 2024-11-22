ff72e02ece28966f2441467079938a97df28682a
Andrii <and.klimenkoo@gmail.com>
2024-11-22 14:42:10 +0100

    updated walking_schedule model

--- backend/src/models/walking_schedule.py
+++ backend/src/models/walking_schedule.py
@@ -3,7 +3,7 @@ from src import db
 class WalkingSchedule(db.Model):
     __tablename__ = 'walking_schedules'
     id = db.Column(db.Integer, primary_key=True)
-    animal_id = db.Column(db.Integer, db.ForeignKey('animals.id'), nullable=False)
+    animal_id = db.Column(db.Integer, db.ForeignKey('animals.id', ondelete='CASCADE'), nullable=False)
     start_time = db.Column(db.DateTime, nullable=False)
     end_time = db.Column(db.DateTime, nullable=False)
     
