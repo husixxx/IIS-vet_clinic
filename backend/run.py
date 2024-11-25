d627c7ad2b07b6802cb78e3a3c11e7365384c8b3
husixxx <xhuska03@stud.fit.vutbr.cz>
2024-11-25 13:41:48 +0100

    Fix local

--- backend/run.py
+++ backend/run.py
@@ -4,4 +4,4 @@ from src.app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 10000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=True)
