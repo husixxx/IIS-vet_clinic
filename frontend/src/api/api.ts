0dba4c6999719d86af615b3717797444abcd47f3
husixxx <xhuska03@stud.fit.vutbr.cz>
2024-11-25 19:05:21 +0100

    Fix https

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://https://iis-vet-clinic.onrender.com", // Default base URL
+  baseURL: "https://iis-vet-clinic.onrender.com", // Default base URL
   withCredentials: true, // Povolit posílání cookies přes různé domény
 });
 
